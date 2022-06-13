bb58d5c54d192e9dd82f7266e3ed8c3d1b4c56f3
Ritesh.V <Ritesh.V@A4G383-PC.afourtech.pune>
2022-06-13 17:10:56 +0530

    Testing with prod deployment

--- handler.py
+++ handler.py
@@ -3,7 +3,7 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "Hello World! Good evening team!",
+        "message": "Hello World! Testing with Manual approval for prod deploy",
         "input": event,
     }
 
