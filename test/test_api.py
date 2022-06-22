bb2b18aefdccbc8f8ec1c4fae3e95fa5d8ddf9f7
Ritesh.V <Ritesh.V@A4G383-PC.afourtech.pune>
2022-06-22 22:01:48 +0530

    working on run automation resolution

--- test/test_api.py
+++ test/test_api.py
@@ -3,17 +3,17 @@ import os
 import pytest
 
 
-@pytest.fixture()
-def api_url():
-    """Fixture that returns a static weather data."""
-    env_key = os.environ['stage']
-    api_url = ""
-
-    if env_key == "stage":
-        api_url = "https://u7wmksc2aa.execute-api.us-east-2.amazonaws.com/dev/courses"
-        return api_url
+# @pytest.fixture()
+# def api_url():
+#     """Fixture that returns a static weather data."""
+#     env_key = os.environ['env_stage']
+#     api_url = ""
+#
+#     if env_key == "stage":
+#         api_url = "https://u7wmksc2aa.execute-api.us-east-2.amazonaws.com/dev/courses"
+#         return api_url
 
 
 def test_get_courses_check_status_code_equals_200(api_url):
-    response = requests.get(api_url)
+    response = requests.get("https://u7wmksc2aa.execute-api.us-east-2.amazonaws.com/dev/courses")
     assert response.status_code == 200
