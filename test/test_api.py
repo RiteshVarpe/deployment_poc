1efbd04acd9185407fa0cea597f310ba326b81d4
Ritesh.V <Ritesh.V@A4G383-PC.afourtech.pune>
2022-06-23 12:10:56 +0530

    modified yaml file to deploy resources as per stages defined in environment

--- test/test_api.py
+++ test/test_api.py
@@ -3,16 +3,6 @@ import os
 import pytest
 
 
-# @pytest.fixture()
-# def api_url():
-#     """Fixture that returns a static weather data."""
-#     env_key = os.environ['env_stage']
-#     api_url = ""
-#
-#     if env_key == "stage":
-#         api_url = "https://u7wmksc2aa.execute-api.us-east-2.amazonaws.com/dev/courses"
-#         return api_url
-
 @pytest.fixture
 def api_url():
     return os.environ.get('api_url')
